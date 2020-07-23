1a6c950a333e80bf34a31e4d95c88e8ee4a0d1c1
AustinPotts <pottsevolvements@gmail.com>
2020-07-23 16:59:43 -0400

    CALL + RET

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,13 +8,14 @@ PRN = 0b01000111
 MUL = 0b10100010
 NOP = 0b00000000
 POP = 0b01000110
-RET = 0b00010001
-CALL = 0b01010000
+RET = 0b00010001 # opcode 10
+CALL = 0b01010000 # opcode 9 
 PUSH = 0b01000101
 SP = 0b00000111
 ADD = 0b10100000
 PUSH = 0b01000101
 POP = 0b01000110
+
 
 
 # Why is it called LS8? 
@@ -26,6 +27,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        # Why can't the LS8 have more ram? (memory)
+        # A: Becuase of our 8 bit bus, our CPU has to communicate with RAM
+        # In order to do that we've set our CPU up with a bunch of wires connecting to RAM (Data Bus, Memory Bus, System Bus, Address Bus)
+        # We use our Data bus ()
         self.ram = [0] * 256 # 255 bytes or in /hex FF
         self.reg = [0] * 8 # 8 Registers, LS8 8 Bit Instruction
         self.flag = 0
@@ -183,15 +188,21 @@ class CPU:
                self.reg[operand_a] = self.reg[operand_a] * self.reg[operand_b]    
                # Increment place counter else infinite loop   
                self.pc += 3
+           elif command == ADD:
+                operand_a = self.ram_read(self.pc + 1)
+                operand_b = self.ram_read(self.pc + 2)
+                self.reg[operand_a] = self.reg[operand_a] + self.reg[operand_b]
+                self.pc += 3
+
            elif command == PUSH:
                  # print("PUSHIN")
                  # get the register number
                  # operand_a = self.ram_read(self.pc + 1)
                  operand_a = operand_a #Register whose value is to be pushed on stack
                  
-                 # Ram Write the Stack Pointer & operand_a
+                 # Write to Ram the Stack Pointer (R7) & operand_a
                  self.ram_write(self.SP, self.reg[operand_a]) 
-                 
+
                  self.pc += 2 # Increment by 2 because this is a 2 byte operation
 
                  # decrement the stack pointer
@@ -203,6 +214,63 @@ class CPU:
                self.reg[operand_a] = self.ram_read(self.SP+1)
                self.SP += 1
                self.pc += 2
+           elif command == CALL: # Calls a subrotuine(function) at the address stored in register 
+               # The PC is set to the address in the given register, we then JUMP to that location in RAM
+               # How to pass parameters when we CALL? 
+               # Where do we store the Data? (Register: will get overwrite so we use the Stack)
+               # IF WE CALL ---- WE HAVE TO RET (Return)
+               #print("CALLIN")
+               # get register number
+               operand_a = self.ram_read(self.pc + 1) # operan_a = reg
+               
+
+               # Figure out the address of our subroutine 
+               # Put that address into a register 
+               # get the address to jump to from the register
+               address = self.reg[operand_a]
+
+               # push command after CALL on the Stack (we use stacks because register might be overwritten in subroutine)
+               return_address = self.pc + 2 # IF this = 2 then the PC is 8, is that correct?
+               # You want this to be 8
+
+               #print(self.pc, self.pc) # 6
+               # Save where we're going back to 
+
+               # decrement stack pointer 
+               # write the value to the memory address that the stack pointer is at
+               self.SP -= 1
+               self.ram_write(self.SP, self.pc + 2)
+               
+               sp = self.SP
+               #print(sp, sp) # 6
+               # put the address on the stack
+               self.ram[sp] = return_address
+               #print(return_address, return_address) # 7
+
+               # then look at register, jump to that address 
+               # Run whatever commands are there 
+               self.pc = address
+               #print(address, address) # 24
+
+            # RET (POP OFF THE STACK & JUMP) We JUMP back so we don't hit HALT & so we can continue program execution
+            # RET will POP the return address off of the stack & store it in PC   
+           elif command == RET:
+               #print("RETTIN")
+               # pop the return address off the stack 
+               sp = self.SP # R7
+               self.SP += 1
+               return_address = self.ram[sp] 
+              # print(self.ram[sp])
+              # print(self.reg)
+              # go to return address: set PC to return address 
+
+               self.pc = return_address 
+               # print(self.pc, self.pc) 
+                
+
+
+
+                
 
 
            elif command == HLT:
@@ -210,6 +278,8 @@ class CPU:
            else:
                 print(f"Run2 Did Not Work")
                 sys.exit(1) 
+
+    # Research Turing Complete 
            
 
     # access the RAM inside the CPU object.
